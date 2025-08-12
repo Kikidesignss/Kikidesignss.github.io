2ff1712c00fb5b655532366c0a8c7f749965a8f4
Kiana Nunez <Knunez1808@gmail.com>
2025-08-12 16:46:50 -0400

    Deploy

--- src/vite.config.js
+++ src/vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/Kikidesignss.github.io/',  // <== Replace with your actual repo name here
+  base: '/',
   plugins: [react()],
 });
